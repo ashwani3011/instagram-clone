e4949f24ae1f67040bc7a7bce1a4139c2abeed52
= <=>
2022-02-10 14:32:00 +0530

    suggetions

--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,5 +1,43 @@
+import faker from '@faker-js/faker'
+import { useState, useEffect } from 'react'
+
 function Suggestions() {
-  return <div></div>
+  const [suggestions, setSuggestions] = useState([])
+  useEffect(() => {
+    const suggestions = [...Array(5)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }))
+    // console.log(suggestions)
+    setSuggestions(suggestions)
+  }, [])
+  return (
+    <div className="mt-4 ml-14">
+      <div className="mb-5 flex justify-between text-sm">
+        <h3 className="text-sm font-bold">Suggestions for you</h3>
+        <button className="font-semibold text-gray-600">See All</button>
+      </div>
+      {suggestions.map((profile) => (
+        <div
+          key={profile.id}
+          className="mt-3 flex items-center justify-between"
+        >
+          <img
+            className="h-10 w-10 rounded-full border p-[2px]"
+            src={profile.avatar}
+            alt=""
+          />
+
+          <div className="ml-4 flex-1">
+            <h2 className="text-sm font-semibold">{profile.username}</h2>
+            <h3 className="text-xs text-gray-400">{profile.name}</h3>
+          </div>
+
+          <button className="text-sm font-bold text-blue-400">Follow</button>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Suggestions
